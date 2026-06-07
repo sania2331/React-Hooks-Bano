25504148f684b05d8a5254f0b218b6e29cd55913
sania2331 <saniaahmed9000@gmail.com>
2026-06-07 06:13:45 -0700

    new update about hooks

---
+++ Flow react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import UseRef from "./Hooks/UseRef"
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Welcome Hooks</h1>
+    <UseRef/>
+      </>
+    
+  )
+}
+
+export default App
---
+++ Flow react/src/Hooks/UseRef.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UseRef() {
+  return (
+    <div>
+        <h1>Hello Reference</h1>
+    </div>
+  )
+}
+
+export default UseRef
